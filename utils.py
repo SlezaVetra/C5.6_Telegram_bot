8930a8134fe708756f51050b5aa054088b70cca6
SlezaVetra <78782336+SlezaVetra@users.noreply.github.com>
2021-04-18 21:40:09 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -25,7 +25,7 @@ class CryptoConverter:
         except ValueError:
             raise ConvertionException(f'Не удалось обработать количество {amount}')
 
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        r = requests.get(f'http://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
         total_base = json.loads(r.content)[keys[base]]
 
         return total_base
